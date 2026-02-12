20e19156128d9dcdde35b9e8cbf59473ccfe5015
Merielly Santana <santana.meri10@gmail.com>
2026-02-12 19:07:31 -0300

    Fix create property validation messages and add user creation success response

--- vizin/DTO/Property/PropertyCreateDto.cs
+++ vizin/DTO/Property/PropertyCreateDto.cs
@@ -17,17 +17,17 @@ public class PropertyCreateDto
     
     [Required(ErrorMessage = "Valor da diária é obrigatório")]
     [Range(1, 100000)]
-    public decimal DailyValue { get; set; }
+    public decimal? DailyValue { get; set; }
 
     [Required(ErrorMessage = "Capacidade é obrigatória")]
     [Range(1, 50, ErrorMessage = "A capacidade deve estar entre 1 e 50")]
-    public int Capacity { get; set; }
+    public int? Capacity { get; set; }
 
     [Required(ErrorMessage = "Tipo da acomodação é obrigatório")]
     [Range(1, 3)]
-    public int AccomodationType { get; set; }
+    public int? AccomodationType { get; set; }
 
     [Required(ErrorMessage = "Categoria da propriedade é obrigatório")]
     [Range(1, 4)]
-    public int PropertyCategory { get; set; }
+    public int? PropertyCategory { get; set; }
 }
